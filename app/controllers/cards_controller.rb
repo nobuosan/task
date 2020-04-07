e5004708b3448be5fb46997508c14eca47b80a02
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-04-07 21:04:52 +0900

    add deleting cards custom

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,5 @@
 class CardsController < ApplicationController
-  before_action :set_card, only: %i(show edit update)
+  before_action :set_card, only: %i(show edit update destroy)
   def new
     @card = Card.new
     @list = List.find_by(id: params[:list_id])
@@ -25,6 +25,10 @@ class CardsController < ApplicationController
     else
       render action: :edit
     end
+  end
+  def destroy
+    @card.destroy
+    redirect_to :root
   end
   private
     def card_params
